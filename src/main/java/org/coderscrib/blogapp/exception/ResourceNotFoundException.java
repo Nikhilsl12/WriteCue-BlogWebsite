9e1f9bb526340a2215bd0f6ea9d5b8de245bee6c
Nikhil Singhal <Nikhilsinghal476@gmail.com>
2025-07-09 23:51:10 +0530

    exception add

---
+++ src/main/java/org/coderscrib/blogapp/exception/ResourceNotFoundException.java
@@ -0,0 +1,38 @@
+package org.coderscrib.blogapp.exception;
+
+/**
+ * Exception thrown when a requested resource cannot be found.
+ * This exception is typically used when a database query returns no results
+ * for a given identifier.
+ */
+public class ResourceNotFoundException extends BlogAppException {
+    
+    /**
+     * Constructs a new ResourceNotFoundException with null as its detail message.
+     */
+    public ResourceNotFoundException() {
+        super();
+    }
+    
+    /**
+     * Constructs a new ResourceNotFoundException with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    
+    /**
+     * Constructs a new ResourceNotFoundException with a formatted message.
+     *
+     * @param resourceName the name of the resource that was not found (e.g., "User", "Post")
+     * @param fieldName the name of the field used in the search (e.g., "id", "email")
+     * @param fieldValue the value of the field used in the search
+     * @return a new ResourceNotFoundException with a formatted message
+     */
+    public static ResourceNotFoundException create(String resourceName, String fieldName, Object fieldValue) {
+        return new ResourceNotFoundException(
+                String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+    }
+}
